d02bac020e4ed63f6a2c85eaeba1a6f3ab4937e3
Emack333 <anikenneth333@gmail.com>
2014-08-22 01:07:32 +0100

    My programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -26,17 +26,36 @@ makeCacheMatrix <- function(x = matrix()) {
 # The following function calculates the inverse of the 
 #special "matrix" created with the above function. However,
 # it first checks to see if the inverse has already been calculated. 
-#If so, it gets the inverse from the cache and skips the computation. Otherwise, it calculates the mean of the data and sets the value of the mean in the cache via the setmean function
+#If so, it gets the inverse from the cache and skips the computation. Otherwise, it calculates the mean 
+#of the data and sets the value of the mean in the cache via the setmean function
+
+
+
+#This function gets the inverse of a given matrix, and stores it to be looked up during other same computations  
+
+makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+}
+
+#This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already 
+#been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-		m<-x$getmatrix()
-        if(!is.null(m)){
-                message("getting cached data")
-                return(m)
-        }
-        matrix<-x$get()
-        m<-solve(matrix, ...)
-        x$setmatrix(m)
-        m
+	inverse_cache <- x$getinverse()
+	if(!is.null(inv)) {
+		message("getting cached data.")
+		return(inverse_cache)
+		}
+	data <- x$get()
+	inv <- solve(data)
+	x$setinverse(inverse_cache)
+	inverse_cache
 }
